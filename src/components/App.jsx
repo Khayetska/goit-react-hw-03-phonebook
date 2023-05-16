afbbe6cbafe879b3547fa80ba69f075428befd69
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-05-16 22:26:40 +0300

    Record contacts data to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,6 +58,21 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(preProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
